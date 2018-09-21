d64a5ac320dfae26ac506703c92aa7f4fd512d1e
Nicky Chen <nicky.chen0@gmail.com>
2018-09-20 20:14:17 -0700

    starting sql queries

---
+++ queries.sql
@@ -0,0 +1,9 @@
+ /* To show all Tables: .tables */
+ /*How many total Characters are there?*/
+ select COUNT(character_id) from charactercreator_character ; /* 302 */
+
+ How many of each specific subclass?
+ How many total Items?
+ How many of the Items are weapons? How many are not?
+ On average, how many Items does each Character have?
+ On average, how many Weapons does each character have?
